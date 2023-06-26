a767b5f79315b9725fb6c51c367f6e26df3bc624
Ostap Brehin <osbre@protonmail.com>
2023-06-26 14:57:36 +0100

    Update sample data
    Update sample data

--- app/Repositories/DemoItemRepository.php
+++ app/Repositories/DemoItemRepository.php
@@ -12,6 +12,7 @@ class DemoItemRepository implements ItemRepository
             new ItemData(id: 1, name: 'Item 1'),
             new ItemData(id: 2, name: 'Item 2'),
             new ItemData(id: 3, name: 'Item 3'),
+            new ItemData(id: 4, name: 'Item 4'),
         ];
     }
 
